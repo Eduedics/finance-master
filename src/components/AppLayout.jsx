863fdb4ccffe21dd1f5936a30f9f7c68527eed99
Edward <kemuthii@gmail.com>
2025-04-16 10:02:58 +0300

    added footer

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,11 +1,18 @@
 import BalanceCard from "./BalanceCard";
 import Header from "./Header";
-import { Outlet } from "react-router-dom";
+import RecentTransaction from "./RecentTransaction";
+import SummaryCard from "./SummaryCard";
+import Footer from "./Footer";
+// import { Outlet } from "react-router-dom";
 export default function AppLayout() {
     return (
       <div className="app-container">
         <Header />
-        <Outlet />
+        {/* <Outlet /> */}
+        <BalanceCard />
+        <RecentTransaction />
+        <SummaryCard/>
+        <Footer/>
       </div>
     );
   }

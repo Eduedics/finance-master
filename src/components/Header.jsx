9e41b00bde6bcf0f72e25e92d2c2922af0024204
Edward <kemuthii@gmail.com>
2025-04-16 08:20:05 +0300

    added header

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h1>Finance Master</h1>
+      <p className="subtitle">Track your income, expenses & savings</p>
+    </header>
+  );
+}
